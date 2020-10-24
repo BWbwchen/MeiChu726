ec0df91d98767a210394ee4e6ec33f79a11012c8
107061218 <leoshieh0hn@gmail.com>
2020-10-24 14:35:47 +0800

    add keyboard monitor

--- keyboard_monitor.py
+++ keyboard_monitor.py
@@ -1,20 +1,38 @@
 from pynput import keyboard
+import time
+
+t_start = 0.0
+t_end = 0.0
+delta_t = 0.0
+
+action = []
 
 
 def on_press(key):
+    global t_start
     try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
+        t_start = time.time()
+        print('alphanumeric key {0} pressed'.format(key.char))
+        action.append('alphanumeric key {0} pressed\n'.format(key.char))
     except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+        t_start = time.time()
+        print('special key {0} pressed'.format(key))
+        action.append('special key {0} pressed\n'.format(key))
 
 
 def on_release(key):
-    print('{0} released'.format(
-        key))
+    global t_end, delta_t
+    t_end = time.time()
+    delta_t = t_end - t_start
+    # print('fuck')
+    print('{0} released'.format(key))
+    print(f'duration = {delta_t}')
+    action.append('{0} released\n'.format(key))
+    action.append(f'duration = {delta_t:4f}\n\n')
     if key == keyboard.Key.esc:
         # Stop listener
+        with open('keyboard.log', 'w') as fh:
+            fh.writelines(action)
         return False
 
 
@@ -24,8 +42,8 @@ with keyboard.Listener(
         on_release=on_release) as listener:
     listener.join()
 
-# ...or, in a non-blocking fashion:
-listener = keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release)
-listener.start()
+# # ...or, in a non-blocking fashion:
+# listener = keyboard.Listener(
+#     on_press=on_press,
+#     on_release=on_release)
+# listener.start()
