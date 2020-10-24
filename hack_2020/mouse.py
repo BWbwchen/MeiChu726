084575c919cfd9cce05c08014e63fbe340842a7e
Jack24658735 <liujac0914@gmail.com>
2020-10-24 14:22:08 +0800

    mouse

--- hack_2020/mouse.py
+++ hack_2020/mouse.py
@@ -1,17 +1,25 @@
 from pynput import mouse
 import time
+import sys
+from contextlib import redirect_stdout
+
+FILEPATH = 'log.txt'
+
+def redirect(filePath, val):
+    with open(filePath, 'a') as out:
+        with redirect_stdout(out):
+            print(val)
 
 def on_move(x, y):
-    print('Pointer moved to {0}'.format(
-        (x, y)))
+    print('Pointer moved to {0}'.format((x, y)))
+    redirect(FILEPATH, 'Pointer moved to {0}'.format((x, y)))
 
 def on_click(x, y, button, pressed):
-    print('{0} at {1}'.format(
-        'Pressed' if pressed else 'Released',
-        (x, y)))
-    if not pressed:
-        # Stop listener
-        return False
+    print('{0} at {1}'.format('Pressed' if pressed else 'Released',(x, y)))
+    redirect(FILEPATH, '{0} at {1}'.format('Pressed' if pressed else 'Released',(x, y)))
+    # if not pressed:
+    #     # Stop listener
+    #     return False
 
 def on_scroll(x, y, dx, dy):
     print('Scrolled {0} at {1}'.format(
@@ -19,17 +27,28 @@ def on_scroll(x, y, dx, dy):
         (x, y)))
 
 # Collect events until released
-with mouse.Listener(
-        on_move=on_move,
-        on_click=on_click,
-        on_scroll=on_scroll) as listener:
-    listener.join()
+# with mouse.Listener(
+#         on_move=on_move,
+#         on_click=on_click,
+#         on_scroll=on_scroll) as listener:
+#     listener.join()
 
 # ...or, in a non-blocking fashion:
-listener = mouse.Listener(
-    on_move=on_move,
-    on_click=on_click,
-    on_scroll=on_scroll)
-listener.start()    
+if __name__ == '__main__':
 
-pynput.mouse.Listener.stop
+    # clear the data
+    with open('log.txt', 'w') as f:
+        f.write('\n')
+    listener = mouse.Listener(
+        on_move=on_move,
+        on_click=on_click,
+        on_scroll=on_scroll)
+    listener.start()
+    while True:
+        time.sleep(1)
+        if input() == 'q' or 'Q':
+            mouse.Listener.stop(listener)
+            print('Collect data ends!')
+            break
+
+          
