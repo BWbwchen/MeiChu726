06cf7baff0f3bb13940ffa312da25c6fd43791e8
107061218 <leoshieh0hn@gmail.com>
2020-10-24 20:41:40 +0800

    solve the problem of time offset

--- data_process.py
+++ data_process.py
@@ -1,10 +1,14 @@
 with open('mouse_log.txt', 'r') as fh:
     L = fh.readlines()
+    # print(L)
     new = []
     press = False
+    offset = float(L[1].split(',')[0])
+    # print(offset)
     for action in L[1:]:
         temp = action.split(',')
-        # print(temp)
+        # print(temp[0])
+        temp[0] = str(float(temp[0]) - offset)
         if temp[3] == ' Pressed\n':
             press = True
         if press and temp[3] == ' Moved\n':
